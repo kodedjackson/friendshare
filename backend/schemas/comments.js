8bf85dcb6033489122cc561e1a47274796bad7df
Jackson Jackson <kodedjackson@gmail.com>
2022-09-24 18:50:13 +0100

    I added comments.js

---
+++ backend/schemas/comments.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'comment',
+    title: 'Comment',
+    type: 'documment',
+    feilds: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        },
+    ]
+}
